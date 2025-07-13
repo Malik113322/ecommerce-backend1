943d85791f90c6f0be12faab694174300799566e
Mohd Danish Malik <121788489+Malik113322@users.noreply.github.com>
2025-07-13 12:52:19 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -8,16 +8,6 @@ dotenv.configDotenv();
 
 // paymet getway braintree
 
-
-
-// create-product
-// for image upload
-// cloudinary.config({
-//   cloud_name: 'dzg8wohnb',
-//   api_key: '524223246331484',
-//   api_secret: 'YzzDJCEUjauFXhS-kiYRyD2wLw0',
-// });
-
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME_CLOUDINARY,
   api_key: process.env.PUBLIC_KEY_CLOUDINARY,
@@ -58,7 +48,7 @@ export const getProductController = async (req, res) => {
     const products = await ProductModel.find({})
       .populate("category")
       .sort({ createdAt: -1 });
-    return res.status(200).send({
+       res.status(200).send({
       success: true,
       message: "successfully get products",
       total: products.length,
@@ -66,7 +56,7 @@ export const getProductController = async (req, res) => {
     });
   } catch (error) {
     console.log(error);
-    return res.status(404).send({
+    res.status(404).send({
       success: false,
       message: "products not found",
     });
@@ -77,7 +67,7 @@ export const getProductController = async (req, res) => {
 export const getSingleProductController = async (req, res) => {
   try {
     const product = await ProductModel.find({ slug: req.params.slug });
-    return res.status(200).send({
+      res.status(200).send({
       success: true,
       message: "successfully get single product",
       product,
@@ -105,7 +95,7 @@ export const updateProductController = async (req, res) => {
       { new: true }
     );
 
-    return res.status(200).send({
+      res.status(200).send({
       success: true,
       message: "successfully update",
       product,
@@ -120,7 +110,7 @@ export const deleteProductController = async (req, res) => {
   try {
     const { id } = req.params;
     const product = await ProductModel.findByIdAndDelete(id);
-    return res.status(201).send({
+      res.status(201).send({
       success: true,
       message: "successfully deleted",
       product,
@@ -144,7 +134,7 @@ export const productFilterController = async (req, res) => {
     if (radio.length) args.price = { $gte: radio[0], $lte: radio[1] };
     const products = await ProductModel.find(args);
 
-    return res.status(200).send({
+      res.status(200).send({
       success: true,
       products,
     });
@@ -157,7 +147,7 @@ export const productFilterController = async (req, res) => {
 export const productCountController = async (req, res) => {
   try {
     const total = await ProductModel.find({}).estimatedDocumentCount();
-    return res.status(200).send({
+      res.status(200).send({
       success: true,
       total,
     });
@@ -175,7 +165,7 @@ export const productListController = async (req, res) => {
       .skip((page - 1) * perPage)
       .limit(perPage)
       .sort({ createdAt: -1 });
-    return res.status(200).send({
+      res.status(200).send({
       success: true,
       products,
     });
@@ -196,7 +186,7 @@ export const productSearchController = async (req, res) => {
       ],
     });
 
-    return res.status(200).send({
+      res.status(200).send({
       success: true,
       results,
     });
@@ -216,7 +206,7 @@ export const similarProductController = async (req, res) => {
       .populate("category")
       .limit(2);
 
-    return res.status(200).send(products);
+    res.status(200).send(products);
   } catch (error) {
     console.log(error);
   }
@@ -229,7 +219,7 @@ export const categoryProductController = async (req, res) => {
     const category = await categoryModel.findOne({ slug: req.params.slug });
     const product = await ProductModel.find({ category }).populate("category");
 
-    return res.status(200).send({
+      res.status(200).send({
       product,
       category,
     });
